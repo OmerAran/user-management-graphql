0fa189b4ece10cd307d2c8cb9c762fd1b6187650
OmerAran <omerr.aran@gmail.com>
2023-05-09 16:52:23 +0300

    project init

---
+++ src/main/java/com/omeraran/BasicUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.omeraran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicUserManagementApplication.class, args);
+    }
+
+}
