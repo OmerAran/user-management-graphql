28e2139051c256e15969e25ad68c9aced1b92ea7
OmerAran <omerr.aran@gmail.com>
2023-05-10 09:55:52 +0300

    added models

---
+++ src/main/java/com/omeraran/model/UserRequest.java
@@ -0,0 +1,12 @@
+package com.omeraran.model;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+
+    private Long id;
+    private String username;
+    private String mail;
+    private Role role;
+}
