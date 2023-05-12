7ece5c9241c9b95856b317c5c5ee5716cae6451f
OmerAran <omerr.aran@gmail.com>
2023-05-12 12:02:20 +0300

    added section of properties and test

---
+++ src/test/java/com/omeraran/BasicUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeraran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicUserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
